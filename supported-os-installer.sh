adcc877709be73099b87ff006a28fd783da73a2b
Ganesh Anarse <ganesh.anarse@synacor.com>
2024-02-06 16:47:25 +0530

    ZCS-14487: Fixed bugs related to dnsmasq, install-config. Fixed Indentions.

--- supported-os-installer.sh
+++ supported-os-installer.sh
@@ -15,16 +15,18 @@ cat << HELP_EOF
 	"$0" -i <install-config> -l <license> -t <build-artifact> -a <admin-pass> --optional-pkg <pkg1> --optional-pkg <pkg2>
 	options:
 	-h, --help			Help
-	-p, --admin-pass		Admin account password to set. (Required) (No default)
+	-p, --admin-pass		Admin account password to set. (Default: zimbra)
 	-l, --license			License key or License file to be activated on the zimbra server. (Required) (No default)
 	-i, --install-config		The default install config to configure zimbra server. (Default = $(pwd)/zim-install-config)
 	-b, --build-artifact		Build artifact / Tar file path (Required) (Default = Build present in current directory)
-	-n, --hostname          	Hostname for the server. (Default = $(hostname -f))
+	-n, --hostname		  	Hostname for the server. (Default = $(hostname -f))
 	-d, --domain-name		Domain name for zimbra installation. (Default: DOMAIN_NAME = HOSTNAME)
 	-t, --time-zone			Time zone required to install. (Default: Asia/Singapore)
-        --optional-pkg           	Additional package apart from default packages to install 
-                                	Default: "zimbra-ldap zimbra-logger zimbra-mta zimbra-dnscache zimbra-snmp zimbra-store zimbra-apache zimbra-spell zimbra-convertd zimbra-memcached zimbra-proxy zimbra-archiving zimbra-onlyoffice zimbra-license-daemon"
-					Optional pkgs: Default pkgs + zimbra-modern-ui zimbra-modern-zimlets zimbra-zimlet-document-editor zimbra-zimlet-classic-document-editor zimbra-patch zimbra-mta-patch zimbra-proxy-patch zimbra-ldap-patch
+	--optional-pkg		   	Additional package apart from default packages to install 
+					Default pkgs: zimbra-ldap zimbra-logger zimbra-mta zimbra-snmp zimbra-store zimbra-apache zimbra-spell 
+				        zimbra-convertd zimbra-memcached zimbra-proxy zimbra-archiving zimbra-onlyoffice zimbra-license-daemon"
+					Optional pkgs: zimbra-modern-ui zimbra-modern-zimlets zimbra-zimlet-document-editor zimbra-zimlet-classic-document-editor 
+					 zimbra-patch zimbra-mta-patch zimbra-proxy-patch zimbra-ldap-patch
 HELP_EOF
 }
 
@@ -37,21 +39,21 @@ function main {
 	INSTALL_CONFIG="$dir_script/zim-install-config"
 
 	HOSTNAME="$(hostname -f)"
-	HOST_IP="$(hostname -I | cut -f1 -d" " | tr -d '[:space:]')"
+	HOST_IP="$(curl -s ifconfig.me)"
+	# HOST_IP="$(hostname -I | cut -f1 -d" " | tr -d '[:space:]')"
 	DOMAIN_NAME="$HOSTNAME"
 	ADMIN_PASS="zimbra"
 	TIMEZONE="Asia/Singapore"
 	CURRENT_USER="$(whoami)"
 	sys_mem_kb="$(cat /proc/meminfo | grep MemAvailable | awk '{print $2}')"
 	SYSTEM_MEMORY_AVAIL="$(expr $sys_mem_kb / 1024 / 1024)"
-    # get version from tar file 10.0.0/8.0.0/9.0.0
+	# get version from tar file 10.0.0/8.0.0/9.0.0
 	ZCS_VERSION="$(echo $BUILD_ARTIFACT | grep -oP "(?<=-)\d+(\.\d+)+")" 
 	ver_pattern="10\.\([1-9]\|[1-9][0-9]\)\.*" # matches 10.1x.xx
-    INSTALL_PKGS=(
-        zimbra-ldap
+	INSTALL_PKGS=(
+		zimbra-ldap
 		zimbra-logger
 		zimbra-mta
-		zimbra-dnscache
 		zimbra-snmp
 		zimbra-store
 		zimbra-apache
@@ -62,7 +64,7 @@ function main {
 		zimbra-archiving
 		zimbra-onlyoffice
 		zimbra-license-daemon
-    )
+	)
 
 	redhat_release="false"
 	command -v yum &>/dev/null
@@ -115,8 +117,8 @@ function main {
 			;;
 			*)
 			echo "Invalid argument(s): $2"
-            echo -e "\n\n\n"
-            print_help && exit 1
+			echo -e "\n\n\n"
+			display_options && exit 1
 			;;
 		esac
 	done
@@ -133,32 +135,32 @@ function main {
 function check_vars {
 	[[ -z "$INSTALL_CONFIG" ]] && echo "Install defaults file path not defined." && exit 1;
 
-    # Check if license key of file is given, validate format
-    if [[ -f "$LICENSE" ]]; then
-        echo "License file is provided"
-        # Check if version is 10.1x.xx
-        if echo "$ZCS_VERSION" | grep $ver_pattern >> /dev/null; then
-            echo "$ZCS_VERSION Does not s2upport license in file format. Use license key instead."
-            exit 1;
-        else 
-            license_file="$LICENSE"
-        fi
-    else
-        echo "License key is provided"
-        # Check if version is 10.1x.xx
-        if echo "$ZCS_VERSION" | grep $ver_pattern >> /dev/null; then
-            # Check if license key is in valid format
-            if echo "$LICENSE" | grep '^[0-9]\+$' >> /dev/null; then
-                license_key="$LICENSE"
-            else
-                echo "Invalid key format: $LICENSE License key must only contain numbers."
-                exit 1;
-            fi
-        else
-            echo "$ZCS_VERSION Does not support license key. Use license xml file format."
-            exit 1;
-        fi
-    fi
+	# Check if license key of file is given, validate format
+	if [[ -f "$LICENSE" ]]; then
+		echo "License file is provided"
+		# Check if version is 10.1x.xx
+		if echo "$ZCS_VERSION" | grep $ver_pattern >> /dev/null; then
+			echo "$ZCS_VERSION Does not support license in file format. Use license key instead."
+			exit 1;
+		else 
+			license_file="$LICENSE"
+		fi
+	else
+		echo "License key is provided"
+		# Check if version is 10.1x.xx
+		if echo "$ZCS_VERSION" | grep $ver_pattern >> /dev/null; then
+			# Check if license key is in valid format
+			if echo "$LICENSE" | grep '^[0-9]\+$' >> /dev/null; then
+				license_key="$LICENSE"
+			else
+				echo "Invalid key format: $LICENSE License key must only contain numbers."
+				exit 1;
+			fi
+		else
+			echo "$ZCS_VERSION Does not support license key. Use license xml file format."
+			exit 1;
+		fi
+	fi
 
 	[[ ! -f "$BUILD_ARTIFACT" ]] && echo "Cannot locate the build artifact (zcs-*.tgz) file." && exit 1;
 	[[ -z "$HOSTNAME" ]] && echo "Cannot find \$HOSTNAME." && exit 1;
@@ -216,10 +218,16 @@ function install_zimbra {
 	if [[ "$ret_code" -eq 0 ]]; then
 		echo "Zimbra installation and setup completed."
 		echo "Activating License"
-		sudo su - zimbra -c "zmlicense -a"
-		[[ "$?" -ne "0" ]] && echo "License activation failed. Please check the logs."
-		echo "Running post-install configuration and checks."
-		
+		# Need a logic to activate or skip licence activation here 
+		if echo "$ZCS_VERSION" | grep $ver_pattern >> /dev/null; then
+			# Version 10.1x.xx, use license key
+			sudo su - zimbra -c zmlicense -a $license_file || \
+			echo "Licence activation failed, check logs."
+		else 
+			# Version 8815/900/1000, Use license file
+			sudo su - zimbra -c zmlicense -a $license_key || \
+			echo "Licence activation failed, check logs."
+		fi
 		# Run post install config and print information.
 		postinstall && print_install_complete
 	else
@@ -255,8 +263,7 @@ EOF
 	if [[ -d "$zimbra_dir" ]]; then
 		command -v zmcontrol >> /dev/null
 		if [[ "$?" -ne 0 ]]; then
-			echo -e "Warning: The 'zmcontrol' command and directory still \"$zimbra_dir\" still exists.\n
-					Zimbra might not be completely uninstalled.\n This could affect the new installation."
+			echo -e "Warning: The 'zmcontrol' command and directory still \"$zimbra_dir\" still exists.\nZimbra might not be completely uninstalled.\n This could affect the new installation."
 		fi
 		echo "Zimbra Uninstall is successful."
 	fi
@@ -320,19 +327,19 @@ function install_dns {
 	sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.old || true
 
 	#create the conf file
-	sudo echo -e "server=8.8.8.8\nserver=8.8.4.4\nserver=9.9.9.9\nserver=149.112.112.112\nserver=1.1.1.1\nserver=1.0.0.1\nlisten-address=127.0.0.1\ndomain='$DOMAIN_NAME'\nmx-host='$DOMAIN_NAME','$HOSTNAME',0\naddress=/'$HOSTNAME'/'$MYIP'\n" \
-	>> /etc/dnsmasq.conf
+	nsentry="$(printf "server=8.8.8.8\nserver=8.8.4.4\nserver=9.9.9.9\nserver=149.112.112.112\nserver=1.1.1.1\nserver=1.0.0.1\nlisten-address=127.0.0.1\ndomain='$DOMAIN_NAME'\nmx-host='$DOMAIN_NAME','$HOSTNAME',0\naddress=/$HOSTNAME/$HOST_IP\n")"
+	echo "$nsentry" | sudo tee /etc/dnsmasq.conf
 	local resolv_file="/etc/resolv.conf"
 	grep 'nameserver 8.8.8.8' /etc/resolv.conf >> /dev/null
 	local ret="$(echo $?)"
 	if [[ "$ret" -eq 0 ]]; then
 		echo "Nameserver is correct in $resolv_file"
-    else
+	else
 		# Edit resolve.conf
 		sudo cp /etc/resolv.conf /etc/resolv.conf.old
-        sudo sed -i 's/^nameserver [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+/nameserver 8.8.8.8/' "$resolv_file"
-        echo "Replacement completed!"
-    fi
+		sudo sed -i 's/^nameserver [0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+/nameserver 8.8.8.8/' "$resolv_file"
+		echo "Replacement completed!"
+	fi
 
 	# restart dns services
 	sudo systemctl enable dnsmasq.service > /dev/null 2>&1 \
@@ -394,18 +401,16 @@ function enable_ports {
 
 function update_zimbra_config {
 	# Edit zimbra config files to inject and trigger automated install
+	install_pkgs_string="$(echo ${INSTALL_PKGS[*]})"
 	sed -e "s|placeholder_hostname|$HOSTNAME|g" \
 		-e "s|placeholder_hostip|$HOST_IP|g" \
 		-e "s|placeholder_domainname|$DOMAIN_NAME|g" \
 		-e "s|placeholder_adminpass|$ADMIN_PASS|g" \
 		-e "s|placeholder_timezone|$TIMEZONE|g" \
-		-e "s|placeholder_systemmem|$SYSTEM_MEMORY_AVAIL|g" $INSTALL_CONFIG >> $WORK_DIR/install.conf
-	# Add Pkgs to install
-	install_pkgs_string="$(echo ${INSTALL_PKGS[*]})"
-	sed -e "s|placeholder_install_pkgs|$install_pkgs_string|g" $INSTALL_CONFIG >> $WORK_DIR/install.conf
+		-e "s|placeholder_systemmem|$SYSTEM_MEMORY_AVAIL|g" \
+		-e "s|placeholder_install_pkgs|$install_pkgs_string|g" $INSTALL_CONFIG >> $WORK_DIR/install.conf
 	# Add License key if version => 10.1x.xx
 	if echo "$ZCS_VERSION" | grep $ver_pattern >> /dev/null; then
-		echo "NOTE: Activate license after installation is selected by default for $ZCS_VERSION"
 		echo "LICENSEACTIVATIONOPTION=1" >> $WORK_DIR/install.conf
 		echo "LICENSEKEY=$license_key" >> $WORK_DIR/install.conf
 	fi
@@ -418,7 +423,7 @@ function postinstall {
 	sudo su - zimbra -c "zmcontrol -v" || exit 1;
 	sudo su - zimbra -c "/opt/zimbra/libexec/zmproxyconfig -e -w -C -H $HOSTNAME"
 	local status="$(sudo su - zimbra -c "zmcontrol status")"
-	if [[ $status == 'not running' ]]; then
+	if echo "$status" | grep "not running" >> /dev/null; then
 		echo "Error: Some services are not running. Please check logs." && exit 1
 	else
 		echo "All services are up and running. ZCS auto installation is complete."
@@ -429,8 +434,78 @@ function postinstall {
 
 function do_settings {
 	# Do Optimal security settings
-	# Enable admin proxy
+	echo "Setting optimal security settings"
+	rm -Rf /tmp/provfile
+
+	cat >> /tmp/provfile << EOF
+	mcf zimbraPublicServiceProtocol https
+	mcf zimbraPublicServicePort 443
+	mcf zimbraPublicServiceHostname $HOSTNAME
+	mcf zimbraReverseProxySSLProtocols TLSv1.2
+	mcf +zimbraReverseProxySSLProtocols TLSv1.3
+	mcf zimbraReverseProxySSLCiphers ""
+	mcf +zimbraResponseHeader "Strict-Transport-Security: max-age=31536000; includeSubDomains"
+	mcf +zimbraResponseHeader "X-Content-Type-Options: nosniff"
+	mcf +zimbraResponseHeader "X-Robots-Tag: noindex"
+	mcf +zimbraResponseHeader "Referrer-Policy: no-referrer"
+	mcf zimbraMailKeepOutWebCrawlers TRUE
+	mcf zimbraSmtpSendAddMailer FALSE
+
+	mcf zimbraSSLDHParam /etc/ffdhe4096.pem
+
+	mcf zimbraMtaSmtpdTlsCiphers medium
+	mcf zimbraMtaSmtpdTlsMandatoryCiphers  medium
+	mcf zimbraMtaSmtpdTlsProtocols '>=TLSv1.2'
+	mcf zimbraMtaTlsSecurityLevel may
+
+	ms $HOSTNAME zimbraPop3CleartextLoginEnabled FALSE
+	ms $HOSTNAME zimbraImapCleartextLoginEnabled FALSE
+
+	mcf zimbraLastLogonTimestampFrequency 1s
+	mc default zimbraPrefShortEmailAddress FALSE
+	mc default zimbraFeatureTwoFactorAuthAvailable TRUE
+
+	mcf +zimbraMailTrustedIP 127.0.0.1
+	mcf +zimbraMailTrustedIP $HOST_IP
+	mcf +zimbraGalLdapAttrMap manager=manager
+	mcf zimbraBackupReportEmailSender admin@$DOMAIN_NAME zimbraBackupReportEmailRecipients admin@$DOMAIN_NAME
+
+	ms $HOSTNAME zimbraFileUploadMaxSize 80000000
+	ms $HOSTNAME zimbraMailContentMaxSize 80000000
+	mcf zimbraMtaMaxMessageSize 80000000
+	mcf zimbraFileUploadMaxSize 80000000
+	mcf zimbraMailContentMaxSize 80000000
+EOF
+
 	# Update Authkeys and Syslog
+	sudo su - zimbra -c '/opt/zimbra/bin/postconf -e fast_flush_domains=""'
+	sudo su - zimbra -c '/opt/zimbra/bin/postconf -e smtpd_etrn_restrictions=reject'
+	sudo su - zimbra -c '/opt/zimbra/bin/postconf -e disable_vrfy_command=yes'
+	sudo su - zimbra -c '/opt/zimbra/bin/postconf -e tls_medium_cipherlist=$(/opt/zimbra/common/bin/openssl ciphers)'
+	sudo su - zimbra -c '/opt/zimbra/bin/postconf -e tls_preempt_cipherlist=no'
+
+	sudo su - zimbra -c '/opt/zimbra/bin/zmlocalconfig -e ldap_common_tlsprotocolmin="3.3"'
+	sudo su - zimbra -c '/opt/zimbra/bin/zmlocalconfig -e ldap_common_tlsciphersuite="HIGH"'
+	sudo su - zimbra -c '/opt/zimbra/bin/zmlocalconfig -e ldap_starttls_supported=1'
+	sudo su - zimbra -c '/opt/zimbra/bin/zmlocalconfig -e zimbra_require_interprocess_security=1'
+	sudo su - zimbra -c '/opt/zimbra/bin/zmlocalconfig -e ldap_starttls_required=true'
+
+	sudo su - zimbra -c '/opt/zimbra/bin/zmlocalconfig -e alias_login_enabled=false'
+	sudo su - zimbra -c '/opt/zimbra/bin/zmlocalconfig -e zimbra_same_site_cookie="Strict"'
+
+	sudo su - zimbra -c '/opt/zimbra/bin/zmprov < /tmp/provfile'
+
+	#https://wiki.zimbra.com/wiki/Enabling_Admin_Console_Proxy
+	sudo su - zimbra -c "/opt/zimbra/libexec/zmproxyconfig -e -w -C -H $HOSTNAME"
+
+	# Update Authkeys and Syslog
+	sudo su - zimbra -c '/opt/zimbra/bin/zmupdateauthkeys'
+	sudo /opt/zimbra/libexec/zmsyslogsetup
+
+	sudo su - zimbra -c '/opt/zimbra/bin/zmzimletctl undeploy com_zimbra_clientuploader'
+	sudo rm -Rf /opt/zimbra/lib/ext/com_zimbra_clientuploader
+
+	sudo su - zimbra -c '/opt/zimbra/bin/zmcontrol restart' || exit 1;
 }
 
 function print_install_complete {
